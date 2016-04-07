b6aa5c210cc91190576a5a30fec4f5a0f2d52e79
Martial <martial.jefferson@gmail.com>
2016-04-07 15:06:00 -0400

    add bower components for Timewheel

--- app.js
+++ app.js
@@ -4,18 +4,16 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , map = require('./routes/map')
-  , three = require('./routes/three')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , Mincer = require('mincer');
+    , routes = require('./routes')
+    , map = require('./routes/map')
+    , three = require('./routes/three')
+    , user = require('./routes/user')
+    , http = require('http')
+    , path = require('path');
 
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3050);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -26,12 +24,8 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/papers", express.static(path.join(__dirname, 'public/javascripts/entities')));
 app.use(express.static(path.join(__dirname, 'components')));
-
-var minceEnv = new Mincer.Environment();
-minceEnv.appendPath('public/javascripts/entities');
-
-app.use('/papers', Mincer.createServer(minceEnv));
 
 
 // development only
@@ -40,12 +34,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-//app.get('/login', routes.login);
-// create login view
 app.get('/map', map.get);
 app.get('/three', three.test);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+module.exports = app;
